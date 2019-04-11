e0964a97c2c23171a57fd6cb63abc0a5db3c8c36
hammurapiya <varuar@yandex.ru>
2019-04-11 22:39:02 +0300

    удаляем урок 3

--- lesson3/Task2.py
+++
@@ -1,9 +0,0 @@
-#2. Создайте функцию, принимающую на вход 3 числа и возвращающую наибольшее из них.
-
-list = [1,5,12]
-
-def max_value(par1, par2, par3):
-    return max(par1,par2,par3)
-
-print(max_value(list[0],list[1],list[2]))
-
